0b89c7559cc09ebfb2c68c844c375719b86f142c
Timofey Kondrashin <tkondrashin@icloud.com>
2023-10-04 23:03:26 +0300

    +

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+K
+parsers/cyoa/cyoa_parser.py,4/c/4c3526a616ecf0af9c670075a2a9118269e7651e
+P
+ parsers/cyoa/cyoa_parser_test.py,0/0/005234fb3836473263506026cb1c53cd75d477d7
+O
+parsers/cyoa/output/stories.csv,f/4/f479c8dab9552cfb20e9a9414552dbf8a3aa17b0
---
+++ parsers/cyoa/cyoa_parser_test.py
@@ -0,0 +1,97 @@
+import logging
+
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
+
+# Настройки драйвера
+chrome_options = Options()
+# chrome_options.add_argument("--disable-extensions")
+# chrome_options.add_argument("--disable-gpu")
+# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+
+
+class StoryListParser:
+    def __init__(self):
+        self.story_list = []
+
+    def story_list(self):
+        driver.get("https://chooseyourstory.com/Stories/")
+        story_genres_block_list = driver.find_elements(By.CLASS_NAME, "dark1border")[
+            :-2
+        ]
+        story_genres_link_list = []
+        for story_genre_block in story_genres_block_list:
+            story_genres_link_list.append(
+                story_genre_block.find_elements(By.CLASS_NAME, "categoryBoxExtras")[1]
+                .find_element(By.TAG_NAME, "a")
+                .get_attribute("href")
+            )
+
+        for story_genre_link in story_genres_link_list:
+            driver.get(story_genre_link)
+            genre_story_table = driver.find_elements(By.TAG_NAME, "tbody")[
+                -1
+            ].find_elements(By.TAG_NAME, "tr")
+            story_genre = (
+                driver.find_element(By.TAG_NAME, "h1")
+                .find_element(By.TAG_NAME, "span")
+                .text[2:]
+            )
+            for story_row in genre_story_table[1:]:
+                story_title = story_row.find_elements(By.TAG_NAME, "td")[0].text
+                story_href = (
+                    story_row.find_elements(By.TAG_NAME, "td")[0]
+                    .find_element(By.TAG_NAME, "a")
+                    .get_attribute("href")
+                )
+                story_author = story_row.find_elements(By.TAG_NAME, "td")[1].text
+                story_author_href = (
+                    story_row.find_elements(By.TAG_NAME, "td")[1]
+                    .find_element(By.TAG_NAME, "a")
+                    .get_attribute("href")
+                )
+                story_difficulty = (
+                    story_row.find_elements(By.TAG_NAME, "td")[3]
+                    .find_element(By.TAG_NAME, "img")
+                    .get_attribute("alt")
+                ).split(" - ")[0]
+                story_raiting = story_row.find_elements(By.TAG_NAME, "td")[4].text
+                self.story_list.append(
+                    [
+                        story_genre,
+                        story_title,
+                        story_href,
+                        story_author,
+                        story_author_href,
+                        story_difficulty,
+                        story_raiting,
+                    ]
+                )
+
+    def story_list_csv(self):
+        self.story_list()
+        df = pd.DataFrame(
+            self.story_list,
+            columns=[
+                "genre",
+                "title",
+                "href",
+                "author",
+                "author_href",
+                "difficulty",
+                "raiting",
+            ],
+        )
+        df.to_csv("stories.csv", index=False)
+
+
+parser = StoryListParser()
+parser.story_list_csv()
