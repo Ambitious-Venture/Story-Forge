03bc05a2cc2713449b74326c9aff1c767f43b4e1
Sergey Zelenovskiy <zelnovskiygoodman454@gmail.com>
2023-12-08 21:19:20 +0300

    fix: gradio_utils attempted relative import

--- story_forge/gradio_utils.py
+++ story_forge/gradio_utils.py
@@ -1,5 +1,5 @@
 from typing import List, Callable
-from ..chatbot import ChatBotSession
+from chatbot import ChatBotSession
 from transformers import Conversation
 
 
@@ -17,10 +17,19 @@ def history2conversation(history: List[List[str]]) -> Conversation:
     return Conversation(conversation_history)
 
 
-def gradio_chatbot_func_generator(chatbot: "ChatBotSession") -> Callable:
+def gradio_chatbot_func_generator(chatbot: ChatBotSession, streaming: bool = True) -> Callable:
     def generate_response(user_input: str, history: List[List[str]]) -> str:
         conversation = history2conversation(history)
         chatbot.set_conversation(conversation)
         response = chatbot.send_message(user_input)
         return response
-    return generate_response
+
+    def streaming_generate_response(user_input: str, history: List[List[str]]) -> str:
+        conversation = history2conversation(history)
+        chatbot.set_conversation(conversation)
+        generated_text = ""
+        for new_text in chatbot.stream_generate(user_input):
+            generated_text += new_text
+            yield generated_text
+    
+    return streaming_generate_response if streaming else generate_response
